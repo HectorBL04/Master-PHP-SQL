f74c806206a3cfba1548dacf0c29e90eaf6c7a6e
Héctor B. L <hmanuelbloopez@gmail.com>
2021-11-15 16:25:36 +0000

    Ejemplo PHP hola mundo

---
+++ curso-php/HolaMundo/index.php
@@ -0,0 +1,4 @@
+<h1>CODIGO HTML "HOLA MUNDO!"</h1>
+<?php>
+echo '<h2>CODIGO PHP "HOLA MUNDO!"';
+</?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<title>Título de la WEB</title>
+
+	<head>
+
+		<body>
+			<header>
+				<h1>Hola Mundo CODIOG HTML</h1>
+			</header>
+				<?php
+				echo '<h2> Hola mundo CODIOG PHP </h2>';
+				?>
+		</body>
+</html>
