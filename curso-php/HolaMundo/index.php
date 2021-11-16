9ab8d5eb47bd0e91b78d1c9a051946b52f5b5a4b
Héctor B. L <hmanuelbloopez@gmail.com>
2021-11-16 15:44:59 +0000

    Clase de impresion por php

--- curso-php/HolaMundo/index.php
+++ curso-php/HolaMundo/index.php
@@ -1,4 +1,4 @@
 <h1>CODIGO HTML "HOLA MUNDO!"</h1>
-<?php>
+<?php
 echo '<h2>CODIGO PHP "HOLA MUNDO!"';
-</?>
+?>
---
+++ curso-php/imprime/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+	<html lang="es">
+	<head>
+		<meta chaarset="uft-8"/>
+		<title>Imprimir por pantalla Victor Robles</title>
+	</head>
+	<body>
+	<h1>Master en PHP -victorroblesweb.es</h1>
+		<?php
+			echo '<h3>Listado de videojuegos</h3>';
+			echo "<ul>"
+						."<li>GTA</li>"
+						."<li>Fifa</li>"
+						."<li>Mario Bros</li>"
+						."</ul>";
+			echo "<p>Esta es toda " ."-". " la lista de juegos.</p>";
+		?>
+	</body>
+</html>
